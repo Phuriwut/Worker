71a235ab6c8babde7a91e7becd493df62b75a736
Phuriwut <bun_beat11@hotmail.com>
2020-05-14 10:43:34 +0700

    update enum

--- src/Worker/RegisterWorker.java
+++ src/Worker/RegisterWorker.java
@@ -6,13 +6,12 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
 import Message.Messager;
+import constance.events.ClientEvents;
 import org.json.JSONObject;
 
 import javax.jms.JMSException;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class RegisterWorker extends Worker<Register> implements Runnable {
     public RegisterWorker(String message, Messager messager){
@@ -22,7 +21,7 @@ public class RegisterWorker extends Worker<Register> implements Runnable {
     @Override
     public void run() {
         sendDB();
-        //System.out.println("in message : " +  this.data.getFirstname());
+        System.out.println("in message : " +  this.data.getFirstname());
     }
 
     public void sendDB(){
@@ -31,8 +30,8 @@ public class RegisterWorker extends Worker<Register> implements Runnable {
             PreparedStatement ppsm = database.preparedQuery("INSERT INTO `user`(`firstname`, `lastname`, `email`, `password`, `age`, `career`, `income`, `bankid`, `bankname`, `is_accept`) VALUES (?,?,?,?,?,?,?,?,?,?)");
             ppsm.setString(1,this.data.getFirstname());
             ppsm.setString(2,this.data.getLastname());
-            ppsm.setString(3,data.getEmail());
-            ppsm.setString(4,data.getPassword());
+            ppsm.setString(3,this.data.getEmail());
+            ppsm.setString(4,this.data.getPassword());
             ppsm.setInt(5,this.data.getAge());
             ppsm.setInt(6,this.data.getCareer());
             ppsm.setInt(7,this.data.getIncome());
@@ -47,8 +46,8 @@ public class RegisterWorker extends Worker<Register> implements Runnable {
             notiData.put("detail","☺");
 
             JSONObject dataJSON = new JSONObject();
-            dataJSON.put("type","NOTIFICATE");
-            dataJSON.put("session_id",data.getSession_id());
+            dataJSON.put("type", ClientEvents.NOTIFICATE.getString());
+            dataJSON.put("session_id",this.data.getSession_id());
             dataJSON.put("data",notiData.toString());
 
             this.messager.send(dataJSON.toString());
